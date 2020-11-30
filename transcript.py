d7ceedc40a8571e30be764d9cfe1016e3b65fe9a
CrazyChickenDev <dannychukz15@gmail.com>
2020-11-30 18:51:08 +0100

    update to code sample

--- transcript.py
+++ transcript.py
@@ -1,128 +1,98 @@
-import datetime
-
-#================== FUNCTION DECLARATIONS ==========================
-
-# A function to read data from a text files
-def filereader(filename):
-    if filename.endswith('.txt'):
-        with open(filename, 'r') as f:
-            return f.readlines()
-    else: raise ValueError("File name must end with .txt")
-
-# A function to write data to a text file
-def filewriter(filename, lines):
-    with open(filename, 'a+') as f:
-        return f.writelines(lines)
-
-# A function to print Student's Name
-def getName(fn, ln):
-    return fn+" "+ln
-
-#??????
-def getCount(score, excel, passd, faild):
-    if score > 55 and score <= 100:
-        excel +=1
-    elif score > 45 and score < 55:
-        passd +=1
-    elif score > 0 and score < 45:
-        faild +=1
-
-# A function to determine score grade and remarks
-def getScoreInfo(score):
-    if score > 74 and score <= 100:
-        return str(score)+"\t\t  \tA\t \tExcellent"
-    
-    elif score > 69 and score < 75:
-        return str(score)+"\t\t  \tA\t \tVery Good"
-
-    elif score > 64 and score < 70:
-        return str(score)+"\t\t  \tB\t \tGood"
-        
-    elif score > 59 and score < 65:
-        return str(score)+"\t\t  \tB\t \tCredit"
-        
-    elif score > 54 and score < 60:
-        return str(score)+"\t\t  \tC\t \tCredit"
-        
-    elif score > 49 and score < 55:
-        return str(score)+"\t\t  \tC\t \tPass"
-        
-    elif score > 44 and score < 50:
-        return str(score)+"\t\t  \tD\t \tPass"
-        
-    elif score > 39 and score < 45:
-        return str(score)+"\t\t  \tF\t \tFail"
-        
-    elif score > 0 and score < 45:
-        return str(score)+"\t\t  \tF\t \tFail"
+import subprocess
+from bs4 import BeautifulSoup
+import requests
+from tqdm import tqdm
+import argparse
 
 
-#=================== MAIN PROGRAM BLOCK ============================
+def get_name(course: str, code: str) -> str:
+    url = f"https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-course&dept={course}" \
+          f"&course={code}"
+    res = requests.get(url)
+    html_page = res.content
 
-#Variable decalarations
-rawFileContents, datafeedArray, studentsScores = [], [], []
-runcount, innercount = 0, 0
-yr = datetime.datetime.now()
-generalRemarks = "\nRemarks: You did Good in {} Course(s), Passed {} Course(s) and Failed {} Course(s)\n"
-advice = "\n\tNote: Failed Courses are Advised to be retaken\n\t*******************************************************\n\n"
-fileHeader = "\t\t\tFINAL EXAMINATION RESULTS\n\t\t\t************************\n\t\t\t************************\n"
+    soup = BeautifulSoup(html_page, 'html.parser')
+    text = soup.find_all(text=True)
+    name = text[19][11:-23]
+    formatted = []
+    line = ""
+    for word in name.split():
+        if len(line) + len(word) < 40:
+            line += f" {word}"
+        else:
+            formatted.append(line[1:])
+            line = f" {word}"
+    formatted.append(line[1:])
 
-#Opening the input file
-try:
-    infile = filereader('rawResult.txt')
-except IOError as e:
-    print("File could not be opened; ", e)
-except ValueError as e:
-    print("Unrecognized file name; ", e)
+    name = "\\\\".join(formatted)
+    return name
 
-# Reading and storing the file contents to a list (and getting rid of all form of white spaces)
-for lines in infile:
-    rawFileContents.append(lines.split())
 
-# Creating another list to contain Student's details
-for data in rawFileContents:
-    if data == rawFileContents[0]:
-        pass
-    else:
-        studentsScores.append(data)
+def raw_doc(data, name, add_col):
+    return f"""\\documentclass{{article}}
+\\usepackage[paper=letterpaper,left=20mm,right=20mm,top=3cm,bottom=2cm]{{geometry}}
+\\usepackage[utf8]{{inputenc}}
+\\usepackage{{fancyhdr}}
+\\pagestyle{{fancy}}
+\\usepackage{{longtable}}
+\\usepackage{{makecell}}
+\\usepackage[makeroom]{{cancel}}
 
-# Reading from all the lists and formating the output 
-while runcount < len(rawFileContents):
-    for row in rawFileContents:
-        scoreCounter = 2
-        great, passed, failed = 0, 0, 0
-        for column in row:
-            datafeedArray = studentsScores[runcount]
-            if rawFileContents.index(row) == 0 and row.index(column) == 0:
-                filewriter(getName(datafeedArray[0], datafeedArray[1])+" Transcript.doc", "\n"+fileHeader+"\n")
-                #print(fileHeader)
-                filewriter(getName(datafeedArray[0], datafeedArray[1])+" Transcript.doc", (column+":\t"+getName(datafeedArray[0], datafeedArray[1])+"\n"))
-                #print(column, ":\t", getName(datafeedArray[0], datafeedArray[1]))
-                filewriter(getName(datafeedArray[0], datafeedArray[1])+" Transcript.doc", ("Session: Final\nYear: {}\n\n".format(yr.year))+"\n")
-                #print("Session: Final\nYear: {}\n\n".format(yr.year))
-                filewriter(getName(datafeedArray[0], datafeedArray[1])+" Transcript.doc", "Courses\t    Score\t  Grade\t   Remarks\n=======\t    =====\t  =====\t   =======\n")
-                #print("Courses\t    Score\t  Grade\t   Remarks\n=======\t    =====\t  =====\t   =======\n")
-                
+\\lhead{{{name}}}
+\\rhead{{University of British Columbia}}
+\\renewcommand{{\\headrulewidth}}{{0.4pt}}
+
+\\begin{{document}}
+\\subsection*{{Unofficial Transcript of Records}}
+    \\begin{{longtable}}{{{"|c" if add_col else ""}|l|c| c|  c|c |}}
+    \\hline
+    {"Course Name &" if add_col else ""}Course Code & Grade & Letter & Year of Study & Class Average\\\\\\hline\\hline
+    {data}
+ \\end{{longtable}}
+\\end{{document}}"""
+
+
+def generate_transcript(name, course_names, compile_pdf, data):
+    rows = []
+
+    with open(data, 'r') as f:
+        raw_data = f.read()
+
+    for line in tqdm(raw_data.split('\n')):
+        values = line.split('\t')
+        try:
+            if values[2].strip() == '':
+                print(f'{values[0]} has no grade. Ignoring.')
             else:
-                filewriter(getName(datafeedArray[0], datafeedArray[1])+" Transcript.doc", (column+"\t  "+getScoreInfo(int(datafeedArray[scoreCounter])))+"\n")
-                #print(column, "\t  ", getScoreInfo(int(datafeedArray[scoreCounter])))
-                #counting excellent, pass and failed courses
-                if int(datafeedArray[scoreCounter]) > 55 and int(datafeedArray[scoreCounter]) <= 100: great +=1
-                elif int(datafeedArray[scoreCounter]) > 45 and int(datafeedArray[scoreCounter]) < 55: passed +=1
-                elif int(datafeedArray[scoreCounter]) > 0 and int(datafeedArray[scoreCounter]) < 45: failed +=1
-                else: pass
-                scoreCounter +=1
-                        
-        filewriter(getName(datafeedArray[0], datafeedArray[1])+" Transcript.doc", generalRemarks.format(great, passed, failed))
-        #print(generalRemarks.format(great, passed, failed))
-        filewriter(getName(datafeedArray[0], datafeedArray[1])+" Transcript.doc", advice)        
-        #print(advice)
-        runcount += 1
-        break
-    if runcount == (len(rawFileContents)-1):
-        break
+                if course_names:
+                    rows.append(
+                        f'\\thead{{{get_name(*values[0].split())}}}&{values[0]} & {values[2]} & {values[3]} & {values[7]} & {values[9]} \\\\ \\hline')
+                else:
+                    rows.append(f'{values[0]} & {values[2]} & {values[3]} & {values[7]} & {values[9]} \\\\ \\hline')
+        except IndexError:
+            print(f"Parse error in {line}. Ignoring.")
 
-print("Done...All transcipts have been generated in the resulting folder with respect to the Students")
-                
-    
+    rd = raw_doc('\n'.join(rows), name, course_names)
 
+    with open("transcript.tex", 'w') as f:
+        f.write(rd)
+
+    if compile_pdf:
+        subprocess.call(['pdflatex', 'transcript.tex'])
+        subprocess.call(['rm', 'transcript.log', 'transcript.aux'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str, help='Your full name')
+    parser.add_argument('--data', type=str, default='mydata.txt',
+                        help='Path to the text file containing your grades copied directly from the ssc.'
+                             '\nDefaults to `mydata.txt`'
+                             '\nSee https://github.com/tomginsberg/transcript-generator for more instructions')
+    parser.add_argument('--compile', default=False, help='[flag] If passed compiles pdf with pdflatex',
+                        action='store_true')
+    parser.add_argument('--descriptive', default=False,
+                        help='[flag] If passed add a column of course names fetched from UBC course catalogue',
+                        action='store_true')
+    args = parser.parse_args()
+    generate_transcript(name=args.name, course_names=args.descriptive, compile_pdf=args.compile, data=args.data)
